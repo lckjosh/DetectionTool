54dae3833fd4f6651e8ab529976b232c61a5bf79
Joshua Lim <lckjosh@gmail.com>
2020-07-01 17:03:35 +0800

    fix hook_networks for kernel >4.18

--- include/hook_networks.h
+++ include/hook_networks.h
@@ -1,3 +1,47 @@
 #include "main.h"
+#include <net/net_namespace.h> /* init_net */
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4,18,0)
+#include <linux/proc_fs.h>
+#else
+typedef int (*proc_write_t)(struct file *, char *, size_t);
+#endif
+
+struct proc_dir_entry {
+	atomic_t in_use;
+	refcount_t refcnt;
+	struct list_head pde_openers;
+	spinlock_t pde_unload_lock;
+	struct completion *pde_unload_completion;
+	const struct inode_operations *proc_iops;
+	const struct file_operations *proc_fops;
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4,19,29)
+	const struct dentry_operations *proc_dops;
+#endif
+	union {
+		const struct seq_operations *seq_ops;
+		int (*single_show)(struct seq_file *, void *);
+	};
+	proc_write_t write;
+	void *data;
+	unsigned int state_size;
+	unsigned int low_ino;
+	nlink_t nlink;
+	kuid_t uid;
+	kgid_t gid;
+	loff_t size;
+	struct proc_dir_entry *parent;
+	struct rb_root subdir;
+	struct rb_node subdir_node;
+	char *name;
+	umode_t mode;
+	u8 namelen;
+	char inline_name[];
+};
+
+struct net_entry {
+	const char *name;
+	struct proc_dir_entry *entry;	
+};
 
 int scan_networks(void);
--- src/hook_networks.c
+++ src/hook_networks.c
@@ -1,7 +1,85 @@
 #include "main.h"
+#include "hook_networks.h"
 
 extern int (*core_kern_text)(unsigned long addr); /* Core Kernel Text */
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 18, 0)
+struct net_entry net[6] = {
+    {"tcp", NULL},
+    {"tcp6", NULL},
+    {"udp", NULL},
+    {"udp6", NULL},
+    {"udplite", NULL},
+    {"udplite6", NULL}};
+
+struct proc_dir_entry *find_subdir(struct rb_root *tree, const char *str)
+{
+    struct rb_node *node = rb_first(tree);
+    struct proc_dir_entry *e = NULL;
+
+    while (node)
+    {
+        e = rb_entry(node, struct proc_dir_entry, subdir_node);
+        if (strcmp(e->name, str) == 0)
+            return e;
+        node = rb_next(node);
+    }
+
+    return NULL;
+}
+
+int scan_networks(void)
+{
+    int i, j;
+    unsigned long op_addr[4];
+    unsigned long addr;
+    unsigned char test[12];
+    const struct seq_operations *seq_ops;
+    const struct file_operations *seq_fops;
+    int no_of_net_hooks = 0;
+
+    for (i = 0; i < 6; i++)
+    {
+        net[i].entry = find_subdir(&init_net.proc_net->subdir, net[i].name);
+        if (!net[i].entry)
+            continue;
+
+        seq_ops = net[i].entry->seq_ops;
+        seq_fops = net[i].entry->proc_fops;
+
+        op_addr[0] = (unsigned long)seq_fops->llseek;
+        op_addr[1] = (unsigned long)seq_fops->read;
+        op_addr[2] = (unsigned long)seq_fops->release;
+        op_addr[3] = (unsigned long)seq_ops->show;
+
+        for (j = 0; j < 4; j++)
+        {
+            addr = op_addr[j];
+            if (!core_kern_text(addr))
+            {
+                printk(KERN_ALERT "detection tool: %s function hook detected!\n", net[i].name);
+                no_of_net_hooks++;
+            }
+            else
+            {
+                memcpy(test, (void *)addr, 12);
+                if (test[0] == 0x48 && test[1] == 0xb8 && test[10] == 0xff && test[11] == 0xe0)
+                {
+                    printk(KERN_ALERT "detection tool: %s function hook detected!\n", net[i].name);
+                    no_of_net_hooks++;
+                }
+            }
+        }
+    }
+    if (no_of_net_hooks)
+        printk(KERN_ALERT "detection tool: %d hooked network functions detected!\n", no_of_net_hooks);
+    else
+        printk(KERN_INFO "detection tool: No hooked network functions detected.\n");
+
+    return no_of_net_hooks;
+}
+
+#else
 int scan_networks(void)
 {
     unsigned long addr;
@@ -38,7 +116,7 @@ int scan_networks(void)
                                                                                         \
     if (!core_kern_text(addr))                                                          \
     {                                                                                   \
-        printk(KERN_ALERT "detection tool: %s function hook detected!\n", path);        \
+        printk(KERN_ALERT "detection tool: %s function hook detected!\n", #NAME);       \
         no_of_net_hooks++;                                                              \
     }                                                                                   \
     else                                                                                \
@@ -46,7 +124,7 @@ int scan_networks(void)
         memcpy(test, (void *)addr, 12);                                                 \
         if (test[0] == 0x48 && test[1] == 0xb8 && test[10] == 0xff && test[11] == 0xe0) \
         {                                                                               \
-            printk(KERN_ALERT "detection tool: %s function hook detected!\n", path);    \
+            printk(KERN_ALERT "detection tool: %s function hook detected!\n", #NAME);   \
             no_of_net_hooks++;                                                          \
         }                                                                               \
     }                                                                                   \
@@ -61,6 +139,8 @@ int scan_networks(void)
         printk(KERN_ALERT "detection tool: %d hooked network functions detected!\n", no_of_net_hooks);
     else
         printk(KERN_INFO "detection tool: No hooked network functions detected.\n");
-    
+
     return no_of_net_hooks;
 }
+
+#endif
